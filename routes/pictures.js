ee104fd497990deaaddf91523b357edf2e425557
Bartektrr <109850860+Bartektrr@users.noreply.github.com>
2023-04-03 23:37:27 +0200

    Pictures for auth users

--- routes/pictures.js
+++ routes/pictures.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
-var path = require('path');
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3()
+const { requiresAuth } = require('express-openid-connect');
 // Or
 /* GET pictures listing. */
-router.get('/', async function(req, res, next) {
+router.get('/', requiresAuth(), async function(req, res, next) {
   var params = {
     Bucket: process.env.CYCLIC_BUCKET_NAME,
     Delimiter: '/',
-    Prefix: 'public/'
+    Prefix: req.oidc.user.email + '/'
   };
   var allObjects = await s3.listObjects(params).promise();
   var keys = allObjects?.Contents.map( x=> x.Key)
@@ -27,17 +26,26 @@ router.get('/', async function(req, res, next) {
   res.render('pictures', { pictures: pictures});
 });
 
-router.get('/:pictureName', function(req, res, next) {
-  res.render('pictureDetails', { picture: req.params.pictureName});
+router.get('/:pictureName', requiresAuth(), async function(req, res, next) {
+  let my_file = await s3.getObject({
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "public/" + req.params.pictureName,
+  }).promise();
+  const picture = {
+      src: Buffer.from(my_file.Body).toString('base64'),
+      name: req.params.pictureName
+  }
+  res.render('pictureDetails', { picture: picture});
 });
 
-router.post('/', async function(req, res, next) {  
+router.post('/', requiresAuth(), async function(req, res, next) {  
   const file = req.files.file;
   console.log(req.files);
+  console.log(req.oidc.user);
   await s3.putObject({
     Body: file.data,
     Bucket: process.env.CYCLIC_BUCKET_NAME,
-    Key: "public/" + file.name,
+    Key: req.oidc.user.email + "/" + file.name,
   }).promise()
   res.end();
 });
