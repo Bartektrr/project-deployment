0d7ad3e14464c3e7fd34596ba4f5a34d69a3cc63
Bartektrr <109850860+Bartektrr@users.noreply.github.com>
2023-04-03 02:15:39 +0200

    test

--- routes/pictures.js
+++ routes/pictures.js
@@ -2,18 +2,31 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 var path = require('path');
-
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 // Or
 /* GET pictures listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  // pictures = s3.listObjectsV2(params, function(err, data) {
+  //   if (err) console.log(err, err.stack); // an error occurred
+  //   else     console.log(data);           // successful response
+  // });
   const pictures = fs.readdirSync(path.join(__dirname, '../pictures/'));
   res.render('pictures', { pictures: pictures});
 });
 
-router.post('/', function(req, res, next) {  
+router.get('/:pictureName', function(req, res, next) {
+  res.render('pictureDetails', { picture: req.params.pictureName});
+});
+
+router.post('/', async function(req, res, next) {  
   const file = req.files.file;
   console.log(req.files);
-  fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);
+  await s3.putObject({
+    Body: file.data,
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "public/" + file.name,
+  }).promise()
   res.end();
 });
 
